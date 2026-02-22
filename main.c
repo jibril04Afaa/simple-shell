fbb6e91b257182e01b5c447c41693308a48f6451
Jibril <128831088+jibril04Afaa@users.noreply.github.com>
2026-02-22 00:49:48 -0500

    basic building blocks

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h> // for EXIT_SUCCESS
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    // run command loop
+    sh_loop();
 
+    return EXIT_SUCCESS;
+}
+
+void sh_loop()
+{
+    // this loop handles the commands of the shell
+    char *line;
+    char **args;
+    int status; 
+
+    do
+    {
+        printf("> %c");
+
+        // 1 - read the command from the standard input
+        line = sh_read_line();
+
+        // 2 - parse, by separating the cmd str into a program and arguments
+        args = sh_split_line(args);
+
+        // 3 - run the parsed cmd
+        status = sh_execute(args);
+
+    } while (status);
+    
 }
