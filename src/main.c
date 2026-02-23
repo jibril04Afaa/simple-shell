a99f478f4f2cfa932357c9c6dc3777804db8d364
Jibril <128831088+jibril04Afaa@users.noreply.github.com>
2026-02-23 18:03:23 -0500

    sh_launch complete

--- src/main.c
+++ src/main.c
@@ -154,5 +154,28 @@ int sh_launch(char **args)
     pid_t pid, wpid;
     int status;
 
-    pid = fork
+    pid = fork();
+    if (pid == 0)
+    {
+        // child process
+        if (execvp(args[0], args) == -1)
+        {
+            perror("sh: fork success\n");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        // fork error
+        perror("sh: error forking\n");
+    }
+    else
+    {
+        // parent process
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
 }
